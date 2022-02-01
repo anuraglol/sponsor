85f85a91f2b62b1e83005b28407717e6df881cc7
Anurag <kr.anurag24@gmail.com>
2022-02-01 14:14:05 +0000

    fix: remove og data for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,29 +9,29 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <NextSeo
-        title={`Sponsor ${data.name}`}
-        titleTemplate={`Sponsor ${data.name}`}
-        defaultTitle={`Sponsor ${data.name}`}
-        description={`A website for sponsoring ${data.name}`}
-        canonical="https://www.avneesh.tech/"
-        openGraph={{
-          url: "https://www.avneesh.tech/",
-          title: `Sponsor ${data.name}`,
-          description: `A website for sponsoring ${data.name}`,
-          images: [
-            {
-              url: "/og-image.png",
-              width: 800,
-              height: 420,
-              alt: `Sponsor ${data.name}`,
-            },
-          ],
-        }}
-        twitter={{
-          handle: "@avneesh0612",
-          site: "@avneesh0612",
-          cardType: "summary_large_image",
-        }}
+        title={`Sponsor Anurag`}
+        // titleTemplate={`Sponsor ${data.name}`}
+        // defaultTitle={`Sponsor ${data.name}`}
+        // description={`A website for sponsoring ${data.name}`}
+        // canonical="https://www.avneesh.tech/"
+        // openGraph={{
+        //   url: "https://www.avneesh.tech/",
+        //   title: `Sponsor ${data.name}`,
+        //   description: `A website for sponsoring ${data.name}`,
+          // images: [
+          //   {
+          //     url: "/og-image.png",
+          //     width: 800,
+          //     height: 420,
+          //     alt: `Sponsor ${data.name}`,
+          //   },
+          // ],
+        // }}
+        // twitter={{
+        //   handle: "@avneesh0612",
+        //   site: "@avneesh0612",
+        //   cardType: "summary_large_image",
+        // }}
       />
       <ChakraProvider>
         <Component {...pageProps} />
