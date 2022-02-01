d3f1bbd980a74d44bbcdbcf23df39982ca3bb1dc
Anurag <kr.anurag24@gmail.com>
2022-02-01 14:33:35 +0000

    feat: seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
 import { NextSeo } from "next-seo";
-import data from "../public/data.json";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
@@ -9,29 +8,29 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <NextSeo
-        title={`Sponsor Anurag`}
-        // titleTemplate={`Sponsor ${data.name}`}
-        // defaultTitle={`Sponsor ${data.name}`}
-        // description={`A website for sponsoring ${data.name}`}
-        // canonical="https://www.avneesh.tech/"
-        // openGraph={{
-        //   url: "https://www.avneesh.tech/",
-        //   title: `Sponsor ${data.name}`,
-        //   description: `A website for sponsoring ${data.name}`,
-          // images: [
-          //   {
-          //     url: "/og-image.png",
-          //     width: 800,
-          //     height: 420,
-          //     alt: `Sponsor ${data.name}`,
-          //   },
-          // ],
-        // }}
-        // twitter={{
-        //   handle: "@avneesh0612",
-        //   site: "@avneesh0612",
-        //   cardType: "summary_large_image",
-        // }}
+        title="Sponsor Anurag"
+        titleTemplate="Sponsor Anurag"
+        defaultTitle="Sponsor Anurag"
+        description="A website for sponsoring Anurag"
+        canonical="https://www.anurag.tech/"
+        openGraph={{
+          url: "https://www.anurag.tech/",
+          title: "Sponsor Anurag",
+          description: "A website for sponsoring Anurag",
+          images: [
+            {
+              url: "https://res.cloudinary.com/ddum5vpp3/image/upload/v1643532760/og-image_dwcwhp.png",
+              width: 800,
+              height: 420,
+              alt: `Sponsor Anurag`,
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@kr_anurag_",
+          site: "@kr_anurag_",
+          cardType: "summary_large_image",
+        }}
       />
       <ChakraProvider>
         <Component {...pageProps} />
