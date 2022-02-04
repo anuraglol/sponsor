463386589088d8be1208ac43627d91ae650732de
Anurag <kr.anurag24@gmail.com>
2022-02-04 06:24:35 +0000

    refactor: remove props from checkout component, move them directly to the component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,10 @@
 import type { NextPage } from "next";
 
 import { Checkout } from "../components";
-import { Box, Text, Button, Flex } from "@chakra-ui/react";
+import { Box, Text, Button } from "@chakra-ui/react";
 import Link from "next/link";
 
-import axios from "axios";
-import { useState } from "react";
-import { loadStripe } from "@stripe/stripe-js";
-
-const stripePromise = loadStripe(process.env.stripe_public_key!);
-
 const App: NextPage = () => {
-  const [amount, setAmount] = useState<number>();
-  const [loading, setLoading] = useState<boolean>(false);
-
-  const defaultAmounts = [100, 250, 500];
-
-  const checkoutSession = async () => {
-    const stripe = await stripePromise;
-
-    const checkoutSession = await axios.post("/api/create-checkout-session", {
-      amount: amount,
-    });
-
-    const result = await stripe?.redirectToCheckout({
-      sessionId: checkoutSession.data.id,
-    });
-
-    if (result?.error) {
-      alert(result?.error.message);
-      setLoading(false);
-    }
-  };
-
   return (
     <Box
       display="flex"
@@ -78,14 +50,7 @@ const App: NextPage = () => {
           </a>
         </Link>
       </Box>
-      <Checkout
-        amount={amount}
-        loading={loading}
-        setAmount={setAmount}
-        setLoading={setLoading}
-        checkoutSession={checkoutSession}
-        defaultAmounts={defaultAmounts}
-      />
+      <Checkout />
     </Box>
   );
 };
